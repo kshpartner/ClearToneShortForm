f97b5efce9b40d67feb19608e0bc5d2702f1b290
danma <57510983+kshpartner@users.noreply.github.com>
2026-06-30 14:53:51 +0900

    Update shortform data

--- js/data.js
+++ js/data.js
@@ -7,5 +7,14 @@ window.CLEAR_TONE_SHORTFORMS = [
     "brand": "CLEAR:TONE",
     "title": "공연전에 먹는 클리어톤",
     "description": "이창섭이 공연전에 늘 챙겨먹는 클리어톤"
+  },
+  {
+    "id": "item-mr08c78y-qmtl1",
+    "embedUrl": "https://www.youtube.com/shorts/BgxMtVMO3oo",
+    "image": ".png",
+    "productUrl": "https://cleartoneofficial.com/product/%ED%81%B4%EB%A6%AC%EC%96%B4%ED%86%A4-2%EB%B0%95%EC%8A%A428%ED%8F%AC-26-%ED%95%A0%EC%9D%B8-%EB%AC%B4%EB%A3%8C%EB%B0%B0%EC%86%A1/33/category/1/display/3/?icid=MAIN.product_listmain_2",
+    "brand": "CLEAR_TONE",
+    "title": "이꾸니의 목관리 루틴",
+    "description": "가방속에 챙긴 이꾸니의 목관리 루틴"
   }
 ];
