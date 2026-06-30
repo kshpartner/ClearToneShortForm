581d81300b33a2b78f2175f411140c53bf13432a
danma <57510983+kshpartner@users.noreply.github.com>
2026-06-30 14:23:47 +0900

    Update product image manifest

--- products/manifest.js
+++ products/manifest.js
@@ -1,6 +1,10 @@
 window.CLEAR_TONE_PRODUCTS = [
   {
-    name: "sample-product.svg",
-    label: "샘플 제품"
+    "name": "sample-product.svg",
+    "label": "샘플 제품"
+  },
+  {
+    "name": ".png",
+    "label": "클리어톤_제품이미지"
   }
 ];
