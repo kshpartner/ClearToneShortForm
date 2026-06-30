211890683f0771ae124d13bbe2c33bafc341bfe0
danma <57510983+kshpartner@users.noreply.github.com>
2026-06-30 15:07:08 +0900

    Update shortform data

--- js/data.js
+++ js/data.js
@@ -16,5 +16,14 @@ window.CLEAR_TONE_SHORTFORMS = [
     "brand": "CLEAR:TONE",
     "title": "이꾸니의 목관리 루틴",
     "description": "가방속에 챙긴 이꾸니의 목관리 루틴"
+  },
+  {
+    "id": "item-mr08tu25-e16fn",
+    "embedUrl": "https://www.youtube.com/shorts/pmGgTmZtk1Q",
+    "image": ".png",
+    "productUrl": "https://cleartoneofficial.com/product/%ED%81%B4%EB%A6%AC%EC%96%B4%ED%86%A4-2%EB%B0%95%EC%8A%A428%ED%8F%AC-26-%ED%95%A0%EC%9D%B8-%EB%AC%B4%EB%A3%8C%EB%B0%B0%EC%86%A1/33/category/1/display/3/?icid=MAIN.product_listmain_2",
+    "brand": "CLEAR:TONE",
+    "title": "클리어톤마시는 짱섭",
+    "description": "클리어톤마시는 짱섭 귀여버σ̴̶̷̤ .̫ σ̴̶̷̤"
   }
 ];
