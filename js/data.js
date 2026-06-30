6eab177e562aa1cafc39c7e3f835b7a2d55390b6
danma <57510983+kshpartner@users.noreply.github.com>
2026-06-30 14:44:50 +0900

    Update shortform data

--- js/data.js
+++ js/data.js
@@ -1,12 +1,12 @@
 window.CLEAR_TONE_SHORTFORMS = [
   {
     "id": "original",
-    "embedUrl": "https://www.youtube.com/embed/dQw4w9WgXcQ",
-    "image": "sample-product.svg",
-    "productUrl": "https://example.com/products/original",
-    "brand": "링티 오리지널",
-    "title": "수분을 효과적으로 회복",
-    "description": "SGLT-1을 활용하는 과학적인 배합으로 일상 속 수분 충전을 돕는 솔루션"
+    "embedUrl": "https://www.youtube.com/shorts/5DGRWkWCIW0",
+    "image": ".png",
+    "productUrl": "https://cleartoneofficial.com/product/%ED%81%B4%EB%A6%AC%EC%96%B4%ED%86%A4-2%EB%B0%95%EC%8A%A428%ED%8F%AC-26-%ED%95%A0%EC%9D%B8-%EB%AC%B4%EB%A3%8C%EB%B0%B0%EC%86%A1/33/category/1/display/3/?icid=MAIN.product_listmain_2",
+    "brand": "CLEAR:TONE",
+    "title": "공연전에 먹는 클리어톤",
+    "description": "이창섭이 공연전에 늘 챙겨먹는 클리어톤"
   },
   {
     "id": "kids",
