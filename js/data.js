8983e5918815053822d82c455f73924703f0a53c
danma <57510983+kshpartner@users.noreply.github.com>
2026-06-30 15:01:23 +0900

    Update shortform data

--- js/data.js
+++ js/data.js
@@ -13,7 +13,7 @@ window.CLEAR_TONE_SHORTFORMS = [
     "embedUrl": "https://www.youtube.com/shorts/BgxMtVMO3oo",
     "image": ".png",
     "productUrl": "https://cleartoneofficial.com/product/%ED%81%B4%EB%A6%AC%EC%96%B4%ED%86%A4-2%EB%B0%95%EC%8A%A428%ED%8F%AC-26-%ED%95%A0%EC%9D%B8-%EB%AC%B4%EB%A3%8C%EB%B0%B0%EC%86%A1/33/category/1/display/3/?icid=MAIN.product_listmain_2",
-    "brand": "CLEAR_TONE",
+    "brand": "CLEAR:TONE",
     "title": "이꾸니의 목관리 루틴",
     "description": "가방속에 챙긴 이꾸니의 목관리 루틴"
   }
