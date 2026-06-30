d63153fb5658bd4ba36855fcb8fc9ab6ca20ee5a
danma <57510983+kshpartner@users.noreply.github.com>
2026-06-30 15:11:04 +0900

    Update shortform data

--- js/data.js
+++ js/data.js
@@ -34,5 +34,14 @@ window.CLEAR_TONE_SHORTFORMS = [
     "brand": "CLEAR:TONE",
     "title": "이꾸니의 아침 루틴",
     "description": "아침에 일어나면 제일 먼저 하는 것 🐶🍋\n오늘도 목 관리 가볍게 시작!"
+  },
+  {
+    "id": "item-mr08z740-vdcl5",
+    "embedUrl": "https://www.youtube.com/shorts/uhNUZwPrpvY",
+    "image": ".png",
+    "productUrl": "https://cleartoneofficial.com/product/%ED%81%B4%EB%A6%AC%EC%96%B4%ED%86%A4-2%EB%B0%95%EC%8A%A428%ED%8F%AC-26-%ED%95%A0%EC%9D%B8-%EB%AC%B4%EB%A3%8C%EB%B0%B0%EC%86%A1/33/category/1/display/3/?icid=MAIN.product_listmain_2",
+    "brand": "CLEAR:TONE",
+    "title": "🎤 노래 연습 전 꼭 하는 루틴",
+    "description": "오늘도 클리어톤 한 포와 함께\n즐겁게 연습 시작!"
   }
 ];
